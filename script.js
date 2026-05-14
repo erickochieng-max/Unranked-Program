401e98015d35393322f4f688ce37c1d1a1975cb5
Erick Ochieng <eoochieng044@gmail.com>
2026-05-14 18:27:39 +0300

    finished on the elegibility responsiveness

--- script.js
+++ script.js
@@ -9,7 +9,56 @@ if (elegibilityForm){
         event.preventDefault();
 
         // Values
-        const childName = document.getElementById("child-name")
-    })
+        const childName = document.getElementById("child-name").value;
+        const age = Number(getElementById("age").value);
+        const skillLevel = document.getElementById("skilllevel").value;
+        const gender = document.getElementById("gender").value;
+
+        const result = document.getElementById("result");
+
+        // Validate
+        if (!childName || !age || !skillLevel || !gender){
+
+            result.innerText = "Please fill in.";
+            result.style.color = "green";
+            return;
+                }
+
+                // Age Validate
+                if (age < 5 || age > 18  ){
+
+                    result.innerText =
+                    "Sorry, the program is only for children aged 5 to 18. ";
+
+                    result.style.color = "green";
+                    return;
+                }
+
+                // Training Time
+                let trainingTime ="";
+
+                // Beginner & Intermediate
+                if (
+                    skillLevel === "beginner" ||
+                    skillLevel === "intermediate"
+                ){
+                    trainingTime = "9:00 AM to 11:00 AM";
+                }
+
+                else if (skillLevel === "Elite"){
+                    trainingTime = "11:30 AM to 13:00 AM";
+                }
+
+                // Results
+                result.innerText =
+                childName +
+                "Qualified for the" +
+                skillLevel +
+                "Program and should train from" +
+                trainingTime +
+                "."
+                result.style.color = "green";
+
+    });
 }
    
