f26bbdfde95c33f550110f29402dc8f1e1c217f3
Erick Ochieng <eoochieng044@gmail.com>
2026-05-14 17:23:53 +0300

    linked the elegibility form to js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 window.addEventListener("load", function() {
     console.log("Page fully loaded with all resources!");
 });
+
+// Elegibility Form
+const elegibilityForm = document.getElementById("elegibility-form")
