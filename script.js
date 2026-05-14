747d4778f4f8d3ff27f28d0e2bc8264065d5fa0a
Erick Ochieng <eoochieng044@gmail.com>
2026-05-14 22:58:01 +0300

    made modifications on all pages

--- script.js
+++ script.js
@@ -3,34 +3,29 @@ window.addEventListener("load", function() {
 });
 
 // Elegibility Form
-const elegibilityForm = document.getElementById("elegibility-form");
-if (elegibilityForm){
-    elegibilityForm.addEventListener("submit", function(event){
-        event.preventDefault();
 
+document.getElementById('elegibility-form').addEventListener('submit',function(event){
+    event.preventDefault();
+
+    
         // Values
         const childName = document.getElementById("child-name").value;
-        const age = Number(getElementById("age").value);
+        const age = Number(document.getElementById("age").value);
         const skillLevel = document.getElementById("skilllevel").value;
         const gender = document.getElementById("gender").value;
 
-        const result = document.getElementById("result");
-
+       
         // Validate
         if (!childName || !age || !skillLevel || !gender){
 
-            result.innerText = "Please fill in.";
-            result.style.color = "green";
+            alert("Please fill in the field.")
             return;
                 }
 
                 // Age Validate
                 if (age < 5 || age > 18  ){
 
-                    result.innerText =
-                    "Sorry, the program is only for children aged 5 to 18. ";
-
-                    result.style.color = "green";
+                    alert("Sorry, the program is only for children aged 5 to 18. ")
                     return;
                 }
 
@@ -45,20 +40,26 @@ if (elegibilityForm){
                     trainingTime = "9:00 AM to 11:00 AM";
                 }
 
-                else if (skillLevel === "Elite"){
+                else if (skillLevel === "elite"){
                     trainingTime = "11:30 AM to 13:00 AM";
                 }
 
                 // Results
-                result.innerText =
+                const resultDisplay = document.getElementById("result");
+
+                 resultDisplay.innerText =
                 childName +
                 "Qualified for the" +
                 skillLevel +
                 "Program and should train from" +
                 trainingTime +
                 "."
-                result.style.color = "green";
+                resultDisplay.style.color = "black";
+
+                setTimeout(function(){
+                    location.reload();
+                },15000);
 
     });
-}
+
    
